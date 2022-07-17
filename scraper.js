d4b26ce89a73ea9200048128e8c9078492ae3df4
Louis Ruocco <louisruocco1@gmail.com>
2022-07-17 21:35:39 +0100

    able to send first headline by email

--- scraper.js
+++ scraper.js
@@ -2,6 +2,9 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const nodemailer = require("nodemailer");
 const schedule = require("node-schedule");
+const dotenv=  require("dotenv");
+
+dotenv.config({path: "./.env"});
 
 const url = "https://www.bbc.co.uk/sport/football"
 
@@ -11,9 +14,38 @@ const scrape = async () => {
     const info = {headline: "", description: ""};
     const $ = cheerio.load(data);
     const item = $("div#orb-modules");
-    info.headline = $(item).find("h3").first().text();
+    info.headline = $(item).find("a h3.gs-c-promo-heading__title").first().text();
     info.description = $(item).find("p").first().text();
-    console.log(`Headline: ${info.headline}, Description: ${info.description}`);
+
+    const transporter = nodemailer.createTransport({
+        service: "gmail", 
+        auth: {
+           user: process.env.EMAIL,
+           pass: process.env.PASS
+        }
+    })
+
+    const html = `
+        <h1>BBC Sport</h1>
+        <h3>${info.headline}</h3>
+        <p>${info.description}</p>
+    `
+
+    const options = {
+        from: process.env.EMAIL,
+        to: process.env.EMAIL,
+        subject: "Web Scraper Test",
+        html,
+    };
+
+    transporter.sendMail(options, (err, info) => {
+        if(err){
+            return console.log(err);
+        } else {
+            console.log("sent: " + info.response);
+        }
+    })
+
 }
 
 scrape();
