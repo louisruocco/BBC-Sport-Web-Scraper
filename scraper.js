ee8e77a7b07864f48400cb005fbdacc83c074bdd
Louis Ruocco <louisruocco1@gmail.com>
2022-07-18 11:33:35 +0100

    email set up, schedule still to do

--- scraper.js
+++ scraper.js
@@ -12,10 +12,10 @@ const scrape = async () => {
     const {data} = await axios.get(url);
 
     const mainHeadline = {headline: "", description: "", link: ""};
-    const headline2 = {headline: ""};
-    const headline3 = {headline: ""};
-    const headline4 = {headline: ""};
-    const headline5 = {headline: ""};
+    const headline2 = {headline: "", link: ""};
+    const headline3 = {headline: "", link: ""};
+    const headline4 = {headline: "", link: ""};
+    const headline5 = {headline: "", link: ""};
     const $ = cheerio.load(data);
     const item = $("div#orb-modules");
     
@@ -31,15 +31,26 @@ const scrape = async () => {
     headline2.headline = $(item).find("div.gel-layout__item:nth-child(2) a h3")
         .first()
         .text();
+    headline2.link = $(item).find("div.gel-layout__item:nth-child(2) a")
+        .attr("href")
+        
     headline3.headline = $(item).find("div.gel-layout__item:nth-child(3) a h3")
         .first()
         .text();
+    headline3.link = $(item).find("div.gel-layout__item:nth-child(3) a")
+        .attr("href")
+
     headline4.headline = $(item).find("div.gel-layout__item:nth-child(4) a h3")
         .first()
         .text();
+    headline4.link = $(item).find("div.gel-layout__item:nth-child(4) a")
+        .attr("href")
+
     headline5.headline = $(item).find("div.gel-layout__item:nth-child(5) a h3")
         .first()
         .text();
+    headline5.link = $(item).find("div.gel-layout__item:nth-child(5) a")
+        .attr("href")
 
     const transporter = nodemailer.createTransport({
         service: "gmail", 
@@ -58,10 +69,10 @@ const scrape = async () => {
         <h2>Other Stories:</h2>
         <hr>
         <ul>
-            <li>${headline2.headline}</li>
-            <li>${headline3.headline}</li>
-            <li>${headline4.headline}</li>
-            <li>${headline5.headline}</li>
+            <li><a href="https://bbc.com/${headline2.link}">${headline2.headline}</a></li>
+            <li><a href="https://bbc.com/${headline3.link}">${headline3.headline}</a></li>
+            <li><a href="https://bbc.com/${headline4.link}">${headline4.headline}</a></li>
+            <li><a href="https://bbc.com/${headline5.link}">${headline5.headline}</li>
         </ul>
     `
 
