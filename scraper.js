00529c8ad184751b0198d598a6773860b9a1cc2a
Louis Ruocco <louisruocco1@gmail.com>
2022-07-20 08:13:42 +0100

    changed from cron to object literal syntax

--- scraper.js
+++ scraper.js
@@ -8,7 +8,7 @@ dotenv.config({path: "./.env"});
 
 const url = "https://www.bbc.co.uk/sport/football"
 
-const sevenAM = schedule.scheduleJob("* 7 * * *", async () => {
+const sevenAM = schedule.scheduleJob({hour: 9, minute: 0}, async () => {
     scrape();
 })
 
